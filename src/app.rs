f250525d36ffa80fdc27989c3551eb29887a0271
Garnet DeGelder <garnet@garnetdg.ca>
2024-05-25 09:47:49 -0400

    Move app to separate file

---
+++ src/app.rs
@@ -0,0 +1,130 @@
+use std::{
+    net::{IpAddr, SocketAddr},
+    path::Path,
+    sync::Arc,
+};
+
+use axum::{
+    http::{header, HeaderValue, Method},
+    Router,
+};
+use tokio::net::TcpListener;
+use tokio_util::sync::CancellationToken;
+use tower::ServiceBuilder;
+use tower_http::{
+    catch_panic::CatchPanicLayer,
+    compression::CompressionLayer,
+    cors::CorsLayer,
+    services::{ServeDir, ServeFile},
+    set_header::SetResponseHeaderLayer,
+};
+
+use crate::{
+    api,
+    config::AppConfig,
+    database::Database,
+    services::{
+        audit::AuditService, auth::AuthService, config::ConfigService, users::UsersService,
+    },
+    AppState,
+};
+
+pub struct App {
+    pub state: Arc<AppState>,
+    pub router: Router,
+    pub listener: TcpListener,
+    pub shutdown_token: CancellationToken,
+}
+
+impl App {
+    pub async fn build(config: &AppConfig) -> Self {
+        let shutdown_token = CancellationToken::new();
+
+        let database = Database::new(config);
+
+        let state = Arc::new(AppState {
+            config: config.clone(),
+            shutdown_token: shutdown_token.clone(),
+            config_service: ConfigService::new(&database),
+            audit_service: AuditService::new(&database),
+            auth_service: AuthService::new(&database, config),
+            users_service: UsersService::new(&database, config),
+            database,
+        });
+
+        let host_address = SocketAddr::from((
+            state
+                .config
+                .host
+                .parse::<IpAddr>()
+                .expect("Failed to parse host IP address"),
+            state.config.port,
+        ));
+        let listener = TcpListener::bind(host_address).await.unwrap();
+
+        let static_file_index =
+            Path::new(&state.config.static_file_root).join(state.config.static_file_index.clone());
+
+        let cors_origins: Vec<_> = state
+            .config
+            .cors_allowed_origins
+            .iter()
+            .map(|x| x.parse().unwrap())
+            .collect();
+
+        let router = Router::new()
+            .nest(
+                "/",
+                Router::new()
+                    .route_service("/", ServeFile::new(&static_file_index))
+                    .route_service(
+                        "/*path",
+                        ServeDir::new(&state.config.static_file_root)
+                            .fallback(ServeFile::new(&static_file_index)),
+                    )
+                    .layer(SetResponseHeaderLayer::if_not_present(
+                        header::CACHE_CONTROL,
+                        HeaderValue::from_str(&format!(
+                            "max-age={}",
+                            state.config.http_caching_max_age
+                        ))
+                        .unwrap(),
+                    )),
+            )
+            .nest(
+                "/api",
+                api::route().layer(SetResponseHeaderLayer::if_not_present(
+                    header::CACHE_CONTROL,
+                    HeaderValue::from_static(
+                        "no-store, no-cache, max-age=0, must-revalidate, proxy-revalidate",
+                    ),
+                )),
+            )
+            .layer(
+                ServiceBuilder::new()
+                    .layer(
+                        CorsLayer::new()
+                            .allow_origin(cors_origins)
+                            .allow_credentials(true)
+                            .allow_headers([header::AUTHORIZATION, header::CONTENT_TYPE])
+                            .allow_methods([
+                                Method::GET,
+                                Method::POST,
+                                Method::PUT,
+                                Method::PATCH,
+                                Method::DELETE,
+                            ]),
+                    )
+                    .layer(CatchPanicLayer::new())
+                    .layer(CompressionLayer::new()),
+            )
+            .with_state(state.clone());
+
+        Self {
+            state,
+            listener,
+            router,
+            shutdown_token,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,20 @@
 pub mod api;
+pub mod app;
 pub mod config;
 pub mod database;
 pub mod helpers;
 pub mod services;
 pub mod tasks;
 
-use std::{
-    net::{IpAddr, SocketAddr},
-    path::Path,
-    sync::Arc,
-};
-
-use axum::{
-    http::{header, HeaderValue, Method},
-    Router,
-};
-use tokio::{net::TcpListener, signal};
+use app::App;
+use tokio::signal;
 use tokio_util::sync::CancellationToken;
-use tower::ServiceBuilder;
-use tower_http::{
-    catch_panic::CatchPanicLayer,
-    compression::CompressionLayer,
-    cors::CorsLayer,
-    services::{ServeDir, ServeFile},
-    set_header::SetResponseHeaderLayer,
-};
 
 use config::AppConfig;
 use database::Database;
-use services::{audit::AuditService, auth::AuthService, users::UsersService};
+use services::{
+    audit::AuditService, auth::AuthService, config::ConfigService, users::UsersService,
+};
 use tasks::maintenance;
 
 const CONFIG_FILE: &str = "./config.json";
@@ -37,6 +23,7 @@ pub struct AppState {
     pub config: AppConfig,
     pub database: Database,
     pub shutdown_token: CancellationToken,
+    pub config_service: ConfigService,
     pub audit_service: AuditService,
     pub auth_service: AuthService,
     pub users_service: UsersService,
@@ -46,87 +33,14 @@ pub struct AppState {
 pub async fn main() {
     let config = AppConfig::load(CONFIG_FILE).await;
 
-    let host_address = SocketAddr::from((
-        config
-            .host
-            .parse::<IpAddr>()
-            .expect("Failed to parse host IP address"),
-        config.port,
-    ));
-    let tcp_listener = TcpListener::bind(host_address).await.unwrap();
+    let app = App::build(&config).await;
 
-    let shutdown_token = CancellationToken::new();
+    app.state.audit_service.log(None, "startup");
 
-    let database = Database::new(&config);
+    let maintenance_task = tokio::spawn(maintenance::maintenance_tasks(app.state.clone()));
 
-    let app_state = Arc::new(AppState {
-        audit_service: AuditService::new(&database),
-        auth_service: AuthService::new(&database, &config),
-        users_service: UsersService::new(&database, &config),
-        shutdown_token: shutdown_token.clone(),
-        database,
-        config: config.clone(),
-    });
-
-    let static_file_index = Path::new(&config.static_file_root).join(config.static_file_index);
-
-    let cors_origins: Vec<_> = config
-        .cors_allowed_origins
-        .iter()
-        .map(|x| x.parse().unwrap())
-        .collect();
-
-    let app = Router::new()
-        .nest(
-            "/",
-            Router::new()
-                .route_service("/", ServeFile::new(&static_file_index))
-                .route_service(
-                    "/*path",
-                    ServeDir::new(&config.static_file_root)
-                        .fallback(ServeFile::new(&static_file_index)),
-                )
-                .layer(SetResponseHeaderLayer::if_not_present(
-                    header::CACHE_CONTROL,
-                    HeaderValue::from_str(&format!("max-age={}", config.http_caching_max_age))
-                        .unwrap(),
-                )),
-        )
-        .nest(
-            "/api",
-            api::route().layer(SetResponseHeaderLayer::if_not_present(
-                header::CACHE_CONTROL,
-                HeaderValue::from_static(
-                    "no-store, no-cache, max-age=0, must-revalidate, proxy-revalidate",
-                ),
-            )),
-        )
-        .layer(
-            ServiceBuilder::new()
-                .layer(
-                    CorsLayer::new()
-                        .allow_origin(cors_origins)
-                        .allow_credentials(true)
-                        .allow_headers([header::AUTHORIZATION, header::CONTENT_TYPE])
-                        .allow_methods([
-                            Method::GET,
-                            Method::POST,
-                            Method::PUT,
-                            Method::PATCH,
-                            Method::DELETE,
-                        ]),
-                )
-                .layer(CatchPanicLayer::new())
-                .layer(CompressionLayer::new()),
-        )
-        .with_state(app_state.clone());
-
-    app_state.audit_service.log(None, "startup");
-
-    let maintenance_task = tokio::spawn(maintenance::maintenance_tasks(app_state.clone()));
-
-    axum::serve(tcp_listener, app)
-        .with_graceful_shutdown(shutdown_signal(shutdown_token))
+    axum::serve(app.listener, app.router)
+        .with_graceful_shutdown(shutdown_signal(app.shutdown_token))
         .await
         .expect("Error occurred in web server task");
 
@@ -134,7 +48,7 @@ pub async fn main() {
         .await
         .expect("Error occurred in maintenance task");
 
-    app_state.audit_service.log(None, "shutdown");
+    app.state.audit_service.log(None, "shutdown");
 }
 
 async fn shutdown_signal(shutdown_token: CancellationToken) {
