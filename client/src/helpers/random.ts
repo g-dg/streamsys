bd65882eda3611e8a5a26ac2aa82d2347e128a2d
Garnet DeGelder <garnet@garnetdg.ca>
2024-05-30 17:50:05 -0400

    Add uuid function

--- client/src/helpers/random.ts
+++ client/src/helpers/random.ts
@@ -7,3 +7,19 @@ export function randomString(length: number, chars?: string): string {
     useChars.charAt(Math.floor(Math.random() * useChars.length))
   ).join("");
 }
+
+export function uuid(): string {
+  if (crypto.randomUUID != undefined) {
+    return crypto.randomUUID();
+  }
+
+  function hex(bytes: number): string {
+    let array = new Uint8Array(bytes);
+    crypto.getRandomValues(array);
+    return Array.from(array)
+      .map((byte) => byte.toString(16).padStart(2, "0"))
+      .join("");
+  }
+
+  return hex(4) + "-" + hex(2) + "-" + hex(2) + "-" + hex(2) + "-" + hex(6);
+}
